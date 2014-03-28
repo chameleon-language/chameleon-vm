b8a56eec5da75b04ca54ed33adff6c4478973f47
zs <baloghzsof@gmail.com>
2014-03-28 12:53:50 +0100

    Opcode names (symbolic constants).

--- lib/chameleon/vm.rb
+++ lib/chameleon/vm.rb
@@ -1,3 +1,5 @@
 require "chameleon/vm/version"
 
+require "chameleon/vm/instructions/opcode_names"
+
 require "chameleon/vm/engine"
---
+++ lib/chameleon/vm/instructions/opcode_names.rb
@@ -0,0 +1,15 @@
+module Chameleon
+  module VM
+
+    # input-output
+    I_GETS  = 10
+    I_PUTS  = 11
+
+    # typecasts
+    I_TOI   = 20
+
+    # arithmetics
+    I_ADD   = 30
+
+  end
+end
