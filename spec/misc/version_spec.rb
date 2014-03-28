7dc926a378252bde93f6a7f18422631de3e3b976
zs <baloghzsof@gmail.com>
2014-03-28 11:51:58 +0100

    Spec typo.

--- spec/misc/version_spec.rb
+++ spec/misc/version_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'version' do
-  it 'it is has version number format' do
+  it 'has version number format' do
     expect(Chameleon::VM::VERSION).to match(/\d\.\d\.\d/)
   end
 end
